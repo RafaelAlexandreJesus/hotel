815bf480760f3a4408201c8c4613b6005453fa26
RafaSilvaDev <rafabdasilva12@gmail.com>
2021-07-12 11:53:37 -0300

    ainda não terminei pinha parte, my bro

--- cliente/views.py
+++ cliente/views.py
@@ -1,3 +1,5 @@
+from django.http import request
+from hotel.settings import TEMPLATES
 from django.shortcuts import render
 import conexao
 c = conexao.conectar()
@@ -7,13 +9,20 @@ def index(request):
     return render(request, 'cliente/form_cliente.html')
 
 def insere_cliente(request):
+    nome = request.POST.get('nome')
+    cpf = request.POST.get('cpf')
+    profissao = request.POST.get('profi')
+    tel = request.POST.get('telefone')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
     with c.cursor() as inserir:
-        # nome
-        # cpf
-        # profissao
-        # tel
-        sql = 'insert into cliente(nome, cpf, profissão, telefone) values(%s,%s,%s,%s)'
-        inserir.execute(sql,())
+        sql = 'insert into cliente(nome, cpf, profissao, telefone, email, senha) values(%s,%s,%s,%s,%s,%s)'
+        inserir.execute(sql,(nome, cpf, profissao, tel, email, senha))
+        c.commit()
+    return render(request, 'cliente/form_cliente.html')
 
 def formulario(request):
     return render(request, 'cliente/form_cliente.html')
+
+def home(request):
+    return render(request, '../../home/templates/home/index.html')
