7dfd66a465814f265ac8c1750d4dd74b16b6b27c
RafaelAlexandreJesus <rafagamesax@gmail.com>
2021-07-19 22:01:47 -0300

    Commit do dia 19-07-2021 22:01

--- administracao/views.py
+++ administracao/views.py
@@ -13,5 +13,49 @@ def index(request):
 def formulario(request):
     return render(request, 'administracao/form_cliente.html')
 
+def insere_cliente_adm(request):
+    nome = request.POST.get('nome')
+    cpf = request.POST.get('cpf')
+    profissao = request.POST.get('profi')
+    tel = request.POST.get('telefone')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    with c.cursor() as inserir:
+        sql = 'insert into cliente(nome, cpf, profissao, telefone, email, senha) values(%s,%s,%s,%s,%s,%s)'
+        inserir.execute(sql,(nome, cpf, profissao, tel, email, senha))
+        c.commit()
+    with c.cursor() as listar:
+        sql = "select * from cliente"
+        listar.execute(sql)
+        lista = listar.fetchall()
+    return render(request, 'administracao/index.html', {'listaC': lista})
+
 def editar(request):
-    return render(request, 'administracao/editar_cliente.html')
+    editar = request.POST.get('editar')
+    with c.cursor() as editaF:
+        sql = "select * from cliente where id = %s"
+        editaF.execute(sql, (editar))
+        lista = editaF.fetchall()
+    return render(request, 'administracao/editar_cliente.html', {'listaEdita': lista})
+
+def editar_banco(request):
+    nome = request.POST.get('nome')
+    cpf = request.POST.get('cpf')
+    telefone = request.POST.get('telefone')
+    profi = request.POST.get('profi')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    salva = request.POST.get('salva')
+
+    with c.cursor() as listar:
+        sql = "select * from cliente"
+        listar.execute(sql)
+        lista = listar.fetchall()
+
+    with c.cursor() as editarB:
+        sql = "update cliente set nome = %s, cpf = %s, telefone = %s, profissao = %s, email = %s, senha = %s where id = 1"
+        execucao = editarB.execute(sql, (nome, cpf, telefone, profi, email, senha))
+        if execucao != 0:
+            return render(request, 'administracao/index.html', {'listaC': lista})
+
+    return render(request, 'administracao/editar_cliente.html', {'listaEdita': lista})
