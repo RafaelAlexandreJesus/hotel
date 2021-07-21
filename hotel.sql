3a9c951dd757e72251adb13b96ccc6bf25a793dc
RafaelAlexandreJesus <rafagamesax@gmail.com>
2021-07-21 11:26:02 -0300

    21-07-20201 11:26

--- hotel.sql
+++ hotel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/07/2021 às 16:52
--- Versão do servidor: 10.4.19-MariaDB
--- Versão do PHP: 7.3.28
+-- Tempo de geração: 21-Jul-2021 às 16:25
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,34 +24,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `cliente`
+-- Estrutura da tabela `cliente`
 --
 
 CREATE TABLE `cliente` (
-  `id` int(11) primary key auto_increment,
+  `id` int(11) NOT NULL,
   `nome` varchar(30) NOT NULL,
   `cpf` varchar(20) NOT NULL,
-  `profissao` varchar(40) NOT NULL,
   `telefone` varchar(13) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `senha` varchar(50) DEFAULT NULL
+  `senha` varchar(50) DEFAULT NULL,
+  `profissao` varchar(50) NOT NULL,
+  `tipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `cliente`
+-- Extraindo dados da tabela `cliente`
 --
 
-INSERT INTO `cliente` (`id`, `nome`, `cpf`, `profissao`, `telefone`, `email`, `senha`) VALUES
-(1, 'Rafael Barbosa', '28732987609', 'programador', '19933334444', 'rafabdasilva12@gmail.com', '123');
+INSERT INTO `cliente` (`id`, `nome`, `cpf`, `telefone`, `email`, `senha`, `profissao`, `tipo`) VALUES
+(4, 'Jonaaaas', '52366951809', '19988810189', 'rafael@gmail.com', '123', 'Dev', 1),
+(5, 'Rafael', '52366951809', '19988810189', 'rafa@gmail.com', '123', 'Dev', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `reserva`
+-- Estrutura da tabela `quarto`
+--
+
+CREATE TABLE `quarto` (
+  `numQuarto` int(11) NOT NULL,
+  `status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `quarto`
+--
+
+INSERT INTO `quarto` (`numQuarto`, `status`) VALUES
+(1, 'Ocupado'),
+(2, 'Ocupado'),
+(3, 'Ocupado'),
+(4, 'Ocupado'),
+(5, 'Ocupado'),
+(6, 'Ocupado');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reserva`
 --
 
 CREATE TABLE `reserva` (
-  `id` int(11) primary key auto_increment,
+  `id` int(11) NOT NULL,
   `idcliente` int(11) DEFAULT NULL,
   `dataInicio` varchar(30) NOT NULL,
   `dataFinal` varchar(30) NOT NULL,
@@ -65,24 +90,39 @@ CREATE TABLE `reserva` (
 --
 
 --
--- Índices de tabela `cliente`
+-- Índices para tabela `cliente`
 --
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
 --
--- Índices de tabela `reserva`
+-- Índices para tabela `quarto`
+--
+ALTER TABLE `quarto`
+  ADD PRIMARY KEY (`numQuarto`);
+
+--
+-- Índices para tabela `reserva`
 --
 ALTER TABLE `reserva`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idcliente` (`idcliente`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `quarto`
+--
+ALTER TABLE `quarto`
+  MODIFY `numQuarto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `reserva`
@@ -91,11 +131,11 @@ ALTER TABLE `reserva`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restrições para tabelas despejadas
+-- Restrições para despejos de tabelas
 --
 
 --
--- Restrições para tabelas `reserva`
+-- Limitadores para a tabela `reserva`
 --
 ALTER TABLE `reserva`
   ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`id`);
